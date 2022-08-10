262f7bd02614ce7e8740833d99ceedb890712555
Michael Cheung <ironmic@Michaels-MacBook-Air.local>
2022-08-10 14:16:13 -0400

    card access

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let deck = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '0': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+    'A1': 1,
+    'A11': 11 
+}
+
+const ten = document.getElementById('annoying')
+ten.className = 'card large hK'
